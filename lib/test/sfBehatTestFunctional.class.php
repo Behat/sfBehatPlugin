36b32f389ba4e2b874858190e53da3b37b9d3406
everzet <ever.zet@gmail.com>
2011-03-26 02:28:23 +0200

    fixed issue/2

--- lib/test/sfBehatTestFunctional.class.php
+++ lib/test/sfBehatTestFunctional.class.php
@@ -17,7 +17,24 @@
 class sfBehatTestFunctional extends sfTestFunctional
 {
   /**
-   * Overload sfTestFunctional::shutdown, so it not throws exceptions. 
+   * Overload sfTestFunctional::shutdown, so it not throws exceptions.
    */
   public function shutdown() {}
+
+  /**
+   * Overload to save our config between calls.
+   *
+   * @see sfTestFunctionalBase::call()
+   */
+  public function call($uri, $method = 'get', $parameters = array(), $changeStack = true)
+  {
+    // save old configs
+    $oldconf = sfConfig::getAll(); 
+
+    parent::call($uri, $method, $parameters, $changeStack);
+
+    // restore old configs
+    sfConfig::clear();
+    sfConfig::add($oldconf);
+  }
 }
